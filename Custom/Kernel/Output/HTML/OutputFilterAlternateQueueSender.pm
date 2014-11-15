48c1cf211b58a8dd9bff4021a828c85e99207381
reneeb <info@perl-services.de>
2014-11-15 16:00:06 +0100

    switch outputfilter to otrs4

--- Custom/Kernel/Output/HTML/OutputFilterAlternateQueueSender.pm
+++ Custom/Kernel/Output/HTML/OutputFilterAlternateQueueSender.pm
@@ -12,15 +12,16 @@ package Kernel::Output::HTML::OutputFilterAlternateQueueSender;
 use strict;
 use warnings;
 
-use Kernel::System::Encode;
-use Kernel::System::Ticket;
-use Kernel::System::Time;
-use Kernel::System::DB;
-use Kernel::System::Queue;
-use Kernel::System::QueueSender;
-use Kernel::System::SystemAddress;
+our $VERSION = 0.02;
 
-our $VERSION = 0.01;
+our @ObjectDependencies = qw(
+    Kernel::System::Queue
+    Kernel::System::Ticket
+    Kernel::System::QueueSender
+    Kernel::System::SystemAddress
+    Kernel::System::Web::Request
+    Kernel::Output::HTML::Layout
+);
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -29,52 +30,42 @@ sub new {
     my $Self = {};
     bless( $Self, $Type );
 
-    # get needed objects
-    for my $Object (
-        qw(MainObject ConfigObject LogObject LayoutObject ParamObject)
-        )
-    {
-        $Self->{$Object} = $Param{$Object} || die "Got no $Object!";
-    }
-
-    $Self->{EncodeObject}        = $Param{EncodeObject} || Kernel::System::Encode->new( %{$Self} );
-    $Self->{TimeObject}          = $Param{TimeObject}   || Kernel::System::Time->new( %{$Self} );
-    $Self->{DBObject}            = $Param{DBObject}     || Kernel::System::DB->new( %{$Self} );
-    $Self->{QueueObject}         = $Param{QueueObject}  || Kernel::System::Queue->new( %{$Self} );
-    $Self->{QueueSenderObject}   = Kernel::System::QueueSender->new( %{$Self} );
-    $Self->{SystemAddressObject} = Kernel::System::SystemAddress->new( %{$Self} );
-
-    $Self->{TicketObject} = Kernel::System::Ticket->new( %{$Self} );
-
     return $Self;
 }
 
 sub Run {
     my ( $Self, %Param ) = @_;
 
+    my $ParamObject         = $Kernel::OM->Get('Kernel::System::Web::Request');
+    my $TicketObject        = $Kernel::OM->Get('Kernel::System::Ticket');
+    my $QueueObject         = $Kernel::OM->Get('Kernel::System::Queue');
+    my $QueueSenderObject   = $Kernel::OM->Get('Kernel::System::QueueSender');
+    my $SystemAddressObject = $Kernel::OM->Get('Kernel::System::SystemAddress');
+    my $LayoutObject        = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+
     # get template name
     my $Templatename = $Param{TemplateFile} || '';
     return 1 if !$Templatename;
     return 1 if !$Param{Templates}->{$Templatename};
 
-    my ($TicketID) = $Self->{ParamObject}->GetParam( Param => 'TicketID' );
-    my %Ticket     = $Self->{TicketObject}->TicketGet(
+    my ($TicketID) = $ParamObject->GetParam( Param => 'TicketID' );
+    my %Ticket     = $TicketObject->TicketGet(
         TicketID => $TicketID,
         UserID   => $Param{UserID},
     );
 
-    my %List = $Self->{QueueSenderObject}->QueueSenderGet( QueueID => $Ticket{QueueID} );
+    my %List = $QueueSenderObject->QueueSenderGet( QueueID => $Ticket{QueueID} );
 
     return 1 if !%List;
 
-    my %Queue = $Self->{QueueObject}->QueueGet(
+    my %Queue = $QueueObject->QueueGet(
         ID => $Ticket{QueueID},
     );
 
     my %IDAddressMap;
     my %SenderAddresses;
     for my $ID ( keys %List, $Queue{SystemAddressID} ) {
-        my %Address = $Self->{SystemAddressObject}->SystemAddressGet(
+        my %Address = $SystemAddressObject->SystemAddressGet(
             ID => $ID,
         );
 
@@ -88,14 +79,14 @@ sub Run {
 
     return if !%SenderAddresses;
 
-    my %Queue = $Self->{QueueObject}->QueueGet(
+    my %Queue = $QueueObject->QueueGet(
         ID => $Ticket{QueueID},
     );
 
     my $QueueSystemAddressID = $Queue{SystemAddressID};
     my $SelectedAddress      = $IDAddressMap{ $QueueSystemAddressID };
         
-    my $Select = $Self->{LayoutObject}->BuildSelection(
+    my $Select = $LayoutObject->BuildSelection(
         Data          => \%SenderAddresses,
         Name          => 'From',
         Size          => 1,
